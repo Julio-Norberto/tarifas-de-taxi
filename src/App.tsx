1dc0ae3d179f2f25a88a9902296541d3d8d5395b
Julio-Norberto <juliocsscl@gmail.com>
2023-01-12 19:49:48 -0300

    feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './routes/AppRoutes'
+import { Header } from './components/Header'
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <AppRoutes />
     </BrowserRouter>
   )
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { Taxi, UserCircle } from 'phosphor-react'
+import './header.css'
+
+export const Header: React.FC = () => {
+  return(
+    <header className="header-container">
+      <div className='header-content'>
+        <a href="/"><Taxi size={32} color='#000' /></a>
+
+        <div className='login-header'>
+          <a href="">Login</a>
+          <UserCircle size={22} color='#000' />
+        </div>
+      </div>
+      <div style={{ backgroundColor: '#000', height: '10px' }} />
+    </header>
+  )
+}
