6e3af3d3ad947a02b9a199c1cbb1a8a4cc7ccf25
Julio-Norberto <juliocsscl@gmail.com>
2023-01-12 19:54:09 -0300

    feat: create footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './routes/AppRoutes'
 import { Header } from './components/Header'
 import './App.css'
+import { Footer } from './components/Footer'
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <AppRoutes />
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import './footer.css'
+
+export const Footer: React.FC = () => {
+  return(
+    <footer className='footer-container'>
+      <p>Tarifas de táxi - 2023 - © todos os direitos reservados</p>
+    </footer>
+  )
+}
