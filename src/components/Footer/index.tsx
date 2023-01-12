85ae6ba129cce453929f7dee4963d8518a8bc66d
Julio-Norberto <juliocsscl@gmail.com>
2023-01-12 20:02:08 -0300

    style: fixed footer on bottom and modify the footer text

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import './footer.css'
 export const Footer: React.FC = () => {
   return(
     <footer className='footer-container'>
-      <p>Tarifas de táxi - 2023 - © todos os direitos reservados</p>
+      <p>Tarifas de táxi - Copyright © 2023 - todos os direitos reservados</p>
     </footer>
   )
 }
