29c97659819d808b2c47666d7430ba2e5e735fe1
Julio-Norberto <juliocsscl@gmail.com>
2023-01-13 14:52:36 -0300

    feat: create table with data fetch

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,48 @@
+import { useState, useEffect, EffectCallback } from 'react'
+import axios from 'axios'
 import './home.css'
 
+interface teste {
+  destination: string
+  price: string
+}
+
 export const Home: React.FC = () => {
+  const [pricesInfo, setPricesInfo] = useState<teste[]>()
+
+  useEffect(() => {
+    async function fetchData() {
+      await axios.get('http://localhost:3000/api/prices')
+      .then((res) => {
+        setPricesInfo(res.data)
+        console.log(res.data)
+      })
+    }
+
+    fetchData()
+
+  }, [])
+
   return(
     <section className='home-container'>
-      <h1>Página Home</h1>
+      <h1>Tabela de preços</h1>
+
+      <div>
+        <table border={1}>
+          <thead>
+            <th className='padd' align='left'>Destino</th>
+            <th>Preço</th>
+          </thead>
+          {pricesInfo?.map((princeInfo, index) => (
+            <tbody>
+              <tr style={{ backgroundColor: index % 2 == 0 ? '#ddd' : '' }}>
+                <td height={50}>{pricesInfo[index].destination}</td>
+                <td className='esquerda'>R$ {pricesInfo[index].price}</td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
     </section>
   )
 }
