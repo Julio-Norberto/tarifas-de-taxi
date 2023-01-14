ee22a59575301e1657be1fddcbf80cfd29eac550
Julio-Norberto <juliocsscl@gmail.com>
2023-01-14 14:26:55 -0300

    feat: create table page

--- src/pages/Table/index.tsx
+++ src/pages/Table/index.tsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+interface IPricesInfo {
+  destination: string
+  price: string
+}
+
 export const Tables: React.FC = () => {
+  const [pricesInfo, setPricesInfo] = useState<IPricesInfo[]>()
+
+  useEffect(() => {
+    async function fetchData() {
+      await axios.get('http://localhost:3000/api/prices')
+      .then((res) => {
+        setPricesInfo(res.data)
+        console.log(res.data)
+      })
+    }
+
+    fetchData()
+
+  }, [])
+
   return(
-    <div>
-      <h1>Página Tables</h1>
-    </div>
+    <section className='home-container'>
+      <h1>Cadastro de dados</h1>
+      <div>
+        {pricesInfo ? (
+          <table border={1}>
+            <thead>
+              <tr>
+                <th className='padd' align='left'>Destino</th>
+                <th>Preço</th>
+              </tr>
+            </thead>
+            {pricesInfo?.map((princeInfo, index) => (
+              <tbody key={index}>
+                <tr style={{ backgroundColor: index % 2 == 0 ? '#ddd' : '' }}>
+                  <td height={50}>{pricesInfo[index].destination}</td>
+                  <td className='esquerda'>R$ {pricesInfo[index].price}</td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        ) : 'Ainda não existem dados cadastrados =('}
+      </div>
+    </section>
   )
 }
