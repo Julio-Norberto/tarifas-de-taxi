debeb59e9b1e0a82d6ac95908ae798b3c9971700
Julio-Norberto <juliocsscl@gmail.com>
2023-01-16 21:05:46 -0300

    feat: create login function

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,25 +1,41 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import { Envelope, Key } from 'phosphor-react'
 import './login.css'
 
 export const Login: React.FC = () => {
+  const [login, setLogin] = useState()
+  const [password, setPassword] = useState()
+  const navigate = useNavigate()
+
+  async function handleLogin() {
+    await axios.post('http://localhost:3000/api/login', {
+    login,
+    password,
+    }).then(() => {
+      navigate('/tables')
+    }).catch((err) => console.log(err))
+  }
+
   return (
     <div className='login-container'>
-      <form className='form-card' action="/login" method='post'>
+      <form className='form-card'>
 
         <div className='login-title-div'>
             <h2 className='login-title'>Faça Login</h2>
         </div>
 
         <div className='div-input'>
-            <label className='align'>Seu e-mail <Envelope style={{ marginLeft: 10 }} /> </label>
-            <input type="text" name="email" id="email" placeholder="Digite o seu e-mail..." />
+            <label htmlFor='login' className='align'>Seu Login <Envelope style={{ marginLeft: 10 }} /> </label>
+            <input onChange={(e: any) => setLogin(e.target.value)} type="text" name="login" id="login" placeholder="Digite o seu login..." />
         </div>
 
         <div className='div-input'>
-            <label className='align'>Sua senha <Key style={{ marginLeft: 10 }} /> </label>
-            <input type="password" name="password" id="password" placeholder="Digite sua senha..." />
+            <label htmlFor='password' className='align'>Sua senha <Key style={{ marginLeft: 10 }} /> </label>
+            <input onChange={(e: any) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="Digite sua senha..." />
         </div>
-        <input className='btn-register-login' type="submit" value="Login" />
+        <input onClick={handleLogin} className='btn-register-login' type="button" value="Login" />
 
       </form>
     </div>
