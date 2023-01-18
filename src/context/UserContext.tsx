1b4444bbd914c7e11a109e9aba88ae6b3eeb8095
Julio-Norberto <juliocsscl@gmail.com>
2023-01-18 14:30:38 -0300

    build: add events

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,9 +1,9 @@
 import { createContext } from 'react'
 
-const Context = createContext('arroz docinho')
+const Context = createContext('')
 
 export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <Context.Provider value={''} > {children} </Context.Provider>
+  return <Context.Provider value={'teste'} > {children} </Context.Provider>
 }
 
 export { Context }
