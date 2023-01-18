3a01c60f9f2033c5fa267c59e086f2e69280f082
Julio-Norberto <juliocsscl@gmail.com>
2023-01-18 14:57:30 -0300

    feat: create Message component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './routes/AppRoutes'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
+import { Message } from './components/Message'
 
 import './App.css'
 
@@ -12,6 +13,7 @@ function App() {
     <BrowserRouter>
       <UserProvider>
         <Header />
+        <Message />
         <AppRoutes />
         <Footer />
       </UserProvider>
--- src/hooks/UseFlashMessage.ts
+++ src/hooks/UseFlashMessage.ts
@@ -1,8 +1,8 @@
 import { EventEmitter } from 'events'
-const bus = new EventEmitter()
+export const bus = new EventEmitter()
 
 export const useFlashMessage = () => {
-  function setFlashMessage(msg: string, type: any) {
+  function setFlashMessage(msg: string, type: 'success' | 'err') {
     bus.emit('flash', {
       message: msg,
       type: type
